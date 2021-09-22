da41dffa9a52997c1c89b94b6465c1f77d1c9fcf
PaveL <BPaveL07@yandex.ru>
2021-09-22 21:29:42 +0300

    2

--- src/JavaApplication24.java
+++ src/JavaApplication24.java
@@ -1,7 +1,22 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class JavaApplication24 {
 
 
     public static void main(String[] args) {
+
+        ExecutorService exec = Executors.newFixedThreadPool(5);
+        exec.submit(new Callable<String>() {
+
+            @Override
+            public String call() throws Exception {
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName() + " - done");
+                return null;
+            }
+        });
 
     }
 }
